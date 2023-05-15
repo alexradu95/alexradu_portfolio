819478319fef6bc3753eb15cf28485ef6d73df76
Radu Alexandru <alexradu95@proton.me>
2023-05-15 17:37:59 +0300

    Added threlte

--- src/routes/3d-experiments/+page.svelte
+++ src/routes/3d-experiments/+page.svelte
@@ -1,5 +1,9 @@
 <script>
-    
-</script>
-    
-<h1>Coming soon</h1>
+    import { T } from '@threlte/core'
+  </script>
+  
+<T.Mesh>
+    <T.BoxGeometry args={[1, 2, 1]} />
+    <T.MeshBasicMaterial />
+</T.Mesh>
+  
