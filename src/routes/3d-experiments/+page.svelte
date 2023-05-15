56cf6353e79b38431f85c58ec8f2e909f96ad57b
Radu Alexandru <alexradu95@proton.me>
2023-05-15 19:10:17 +0300

    WIP VR example

--- src/routes/3d-experiments/+page.svelte
+++ src/routes/3d-experiments/+page.svelte
@@ -1,6 +1,22 @@
 <script>
 	import DemoScene from "$lib/3d/DemoScene.svelte";
+    import { useThrelte } from '@threlte/core'
 
+	const { scene, renderer, camera } = useThrelte()
+
+	if (navigator !== undefined) {
+		console.log("Navigator is defined")
+		import('three/examples/jsm/webxr/VRButton').then(module => {
+			const { VRButton } = module;
+			if(renderer) {
+				renderer.xr.enabled = true;
+				document.body.appendChild(VRButton.createButton(renderer));
+				renderer.setAnimationLoop(() => renderer.render(scene, get(camera)))
+			}
+		});
+    } else{
+		console.log("Navigator is not defined");
+	}
 </script>
 
 
