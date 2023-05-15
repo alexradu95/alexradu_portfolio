dac574ea0cc8b7456e3c2b189ca6ccb38edb3d9a
Radu Alexandru <alexradu95@proton.me>
2023-05-15 19:16:22 +0300

    Moved camera upper the hierarchy

--- src/lib/3d/DemoScene.svelte
+++ src/lib/3d/DemoScene.svelte
@@ -15,37 +15,23 @@
   </script>
 
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[10, 10, 10]}
-  on:create={({ ref, cleanup }) => {
-    ref.lookAt(0, 1, 0)
-
-    // provide a cleanup function that is called
-    // when the component is destroyed or `ref`
-    // changes because different `args` are passed.
-    cleanup(() => {
-      console.log('cleanup')
-    })
-  }}
-/>
 
 <T.DirectionalLight castShadow position={[3, 10, 10]} />
 <T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
 <T.AmbientLight intensity={0.2} />
 
-  <!-- Cube -->
-  <T.Group scale={$scale}>
-    <T.Mesh position.y={0.5} castShadow let:ref scale={$scale}
-    on:pointerenter={() => scale.set(1.5)}
-    on:pointerleave={() => scale.set(1)}>
-      <T.BoxGeometry />
-      <T.MeshStandardMaterial color="#333333" />
-    </T.Mesh>
-    <!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-			<T.CircleGeometry args={[3, 72]} />
-			<T.MeshStandardMaterial color="white" />
-		</T.Mesh>
-  </T.Group>
+<!-- Cube -->
+<T.Group scale={$scale}>
+  <T.Mesh position.y={0.5} castShadow let:ref scale={$scale}
+  on:pointerenter={() => scale.set(1.5)}
+  on:pointerleave={() => scale.set(1)}>
+    <T.BoxGeometry />
+    <T.MeshStandardMaterial color="#333333" />
+  </T.Mesh>
+  <!-- Floor -->
+  <T.Mesh receiveShadow rotation.x={degToRad(-90)}>
+    <T.CircleGeometry args={[3, 72]} />
+    <T.MeshStandardMaterial color="white" />
+  </T.Mesh>
+</T.Group>
   
