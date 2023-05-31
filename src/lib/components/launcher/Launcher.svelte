3b5cf55867abc7e179c09247b45ddfc700ca96c7
Alex Radu <radualexandrucosmin@gmail.com>
2023-05-31 19:51:48 +0300

    Fixed launcher

--- src/lib/components/launcher/Launcher.svelte
+++ src/lib/components/launcher/Launcher.svelte
@@ -1,51 +1,50 @@
 <script>
-    import { fade } from 'svelte/transition';
-    import { openedApps, availableApps } from '$lib/data/Applications.js';
-  
-    export const check = (appName) => {
-      for (let i = 0; i < $openedApps.length; i++) {
-        if ($openedApps[i].id === appName) {
-          return false;
-        }
-      }
-    };
-  
-    const openApp = (App, Name) => {
-      console.log('test');
-      if ($openedApps.length === 0 || check(Name) !== false) {
-        $openedApps = [...$openedApps, { id: Name, app: App }];
-        closeLauncher();
-      } else {
-        console.log('already opened');
-      }
-    };
-  
-    const closeLauncher = () => {
-      $openedApps = $openedApps.filter((item) => item.id !== 'Launcher');
-    };
-  </script>
-  
+  import { fade } from 'svelte/transition';
+  import { openedApps, availableApps } from '$lib/data/Applications.js';
 
-  
-  <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div
-    class="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-80"
-    transition:fade
-    on:click|capture|self={closeLauncher}
-  >
-    <div class="relative flex flex-col gap-4">
-      <div class="grid grid-cols-4 grid-rows-6 gap-2 apps-container">
-        {#each $availableApps as app}
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div
-          class="m-5 w-32 h-32 rounded-lg bg-gray-100 bg-opacity-10 flex flex-col items-center justify-center p-2 transition-transform hover:scale-110"
-          on:click={() => openApp(app.component, app.id)}
-        >
-          <div class="text-white text-7xl">{app.icon}</div>
-          <div class="pt-3 text-white text-2xl">{app.id}</div>
-        </div>
-        {/each}
+  const check = (appName) => {
+    for (let i = 0; i < $openedApps.length; i++) {
+      if ($openedApps[i].id === appName) {
+        return false;
+      }
+    }
+  };
+
+  const openApp = (App, Name) => {
+    if ($openedApps.length === 0 || check(Name) !== false) {
+      $openedApps = [...$openedApps, { id: Name, app: App }];
+      closeLauncher();
+    }
+  };
+
+  const closeLauncher = () => {
+    $openedApps = $openedApps.filter((item) => item.id !== 'Launcher');
+  };
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  class="fixed inset-0 flex flex-col justify-start items-center bg-gray-900 bg-opacity-80"
+  transition:fade
+  on:click|capture|self={closeLauncher}
+>
+  <div class="relative mt-1/4 self-center">
+      <div class="grid grid-cols-4 gap-2 mt-32">
+          {#each $availableApps as app}
+          <div
+              class="w-32 h-32 m-5 rounded-lg bg-gray-100 bg-opacity-10 flex flex-col items-center justify-center transition-transform hover:scale-110"
+              on:click={() => openApp(app.component, app.id)}
+          >
+              <div class="text-white text-7xl">{app.icon}</div>
+              <div class="pt-3 text-white text-2xl">{app.id}</div>
+          </div>
+          {/each}
       </div>
-    </div>
   </div>
+</div>
+
+
+
+
+
   
