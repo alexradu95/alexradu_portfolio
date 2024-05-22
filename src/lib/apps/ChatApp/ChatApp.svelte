9bbc5bdfbdf49b10ed3ce061b59d60c55b0e9ada
Radu Alexandru <alexradu95@proton.me>
2024-05-22 20:31:16 +0300

    Commit

--- src/lib/apps/ChatApp/ChatApp.svelte
+++ src/lib/apps/ChatApp/ChatApp.svelte
@@ -15,16 +15,6 @@
 </script>
 
 <App title="Chat Application">
-    <div class="flex flex-col h-full">
-    <div class="m-1 p-2 rounded-lg flex-grow overflow-y-auto relative">
-      <div class="flex flex-col items-center p-4 space-y-4 bg-gray-900 text-white min-h-screen">
-        <div class="w-full max-w-2xl bg-gray-800 shadow-md rounded-lg p-4 flex flex-col space-y-4 h-full relative">
-          <div class="flex-grow overflow-y-auto">
-            <ChatMessages {messages} />
-          </div>
-          <ChatInput {prompt} on:send={handleSend} class="fixed bottom-0 left-0 right-0" />
-        </div>
-      </div>
-    </div>
-  </div>
+    <ChatMessages {messages} />
+    <ChatInput {prompt} on:send={handleSend} class="fixed bottom-0 left-0 right-0" />
 </App>
