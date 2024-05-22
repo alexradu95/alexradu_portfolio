1a4b0b0c2ad5546d0f19696aae594184a9838069
Radu Alexandru <alexradu95@proton.me>
2024-05-23 00:50:07 +0300

    Add height of 100vh to the layout container.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
     import "../app.css";
 </script>
 
-<div style="border: 5px solid orange; padding: 10px;">
+<div style="border: 5px solid orange; padding: 10px; height: 100vh;">
     <slot />
 </div>
