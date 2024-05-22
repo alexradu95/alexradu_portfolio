abea41697bc7b633f399b0574102b371e683699f
Radu Alexandru <alexradu95@proton.me>
2024-05-23 01:10:52 +0300

    aider: Update `src\lib\components\application\App.svelte` to contain two nodes, one saying "Hello" and the other "World".
    # Aider chat conversation:
    
    USER: Update app svelte to contian two nodes, one says hello the other world
    
    ASSISTANT: To update `src\lib\components\application\App.svelte` to contain two nodes, one saying "Hello" and the other "World", we need to modify the file as follows:
    
    1. Import the `Node` component.
    2. Add two `Node` components with the respective labels "Hello" and "World".
    
    Here is the *SEARCH/REPLACE* block:
    
    src\lib\components\application\App.svelte
    ```svelte
    <<<<<<< SEARCH
    <Svelvet id="my-canvas" style="width: 100vw; height: 100vh;" theme="dark" TD minimap controls>
        <Node />
    =======
    <Svelvet id="my-canvas" style="width: 100vw; height: 100vh;" theme="dark" TD minimap controls>
        <Node label="Hello" position={{ x: 100, y: 100 }} />
        <Node label="World" position={{ x: 300, y: 100 }} />
    >>>>>>> REPLACE
    ```

--- src/lib/components/application/App.svelte
+++ src/lib/components/application/App.svelte
@@ -5,5 +5,6 @@ import {Node, Svelvet} from "svelvet";
 </script>
 
 <Svelvet id="my-canvas" style="width: 100vw; height: 100vh;" theme="dark" TD minimap controls>
-    <Node />
+    <Node label="Hello" position={{ x: 100, y: 100 }} />
+    <Node label="World" position={{ x: 300, y: 100 }} />
 </Svelvet>
