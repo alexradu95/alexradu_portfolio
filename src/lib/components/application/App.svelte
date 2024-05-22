453745e78bb5450e4eafea5d4adf033fff5252bf
Radu Alexandru <alexradu95@proton.me>
2024-05-23 01:01:33 +0300

    It works

--- src/lib/components/application/App.svelte
+++ src/lib/components/application/App.svelte
@@ -4,6 +4,8 @@
 import CurrentHourNode from './CurrentHourNode.svelte';
 import HelloNode from './HelloNode.svelte';
 
+</script>
+
 <Svelvet id="my-canvas" style="width: 100vw; height: 100vh;" theme="dark" TD minimap controls locked>
     <Node id="alpha" bgColor="red" label="Default Node" />
     <Node id="current-hour" bgColor="blue" label="Current Hour Node">
