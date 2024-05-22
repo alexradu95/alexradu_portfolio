1f54d81b018a739b76926e75661e7517ab3467d0
Radu Alexandru <alexradu95@proton.me>
2024-05-22 21:10:44 +0300

    Update chat messages container styles with custom scrollbar.

--- src/lib/apps/ChatApp/ChatMessages.svelte
+++ src/lib/apps/ChatApp/ChatMessages.svelte
@@ -5,7 +5,7 @@
   export let messages = writable<Message[]>([]);
 </script>
 
-<div class="flex flex-col space-y-2 p-2 bg-gray-900 text-white h-full overflow-y-auto" id="chatui-chat">
+<div class="flex flex-col space-y-2 p-2 bg-gray-900 text-white h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-900" id="chatui-chat">
   {#each $messages as { kind, text }, index (index)}
     <div class={`flex ${kind === 'right' ? 'justify-end' : 'justify-start'} m-1`} key={index}>
       <div class="w-full p-2 rounded-lg shadow-md bg-gray-700">
