0e033ff0d05e6aa6e05350e4c9256d1da4ccc494
Radu Alexandru <alexradu95@proton.me>
2024-05-23 01:10:51 +0300

    Remove specific widget imports and use a generic Node component instead in App.svelte.

--- src/lib/components/application/App.svelte
+++ src/lib/components/application/App.svelte
@@ -1,16 +1,9 @@
 <script>
 
 import {Node, Svelvet} from "svelvet";
-import DefaultNode from './widgets/DefaultNode.svelte';
-import CurrentHourNode from './widgets/CurrentHourNode.svelte';
-import HelloNode from './widgets/HelloNode.svelte';
-import ExampleNode from './widgets/ExampleNode.svelte';
 
 </script>
 
 <Svelvet id="my-canvas" style="width: 100vw; height: 100vh;" theme="dark" TD minimap controls>
-    <DefaultNode />
-    <CurrentHourNode />
-    <HelloNode />
-    <ExampleNode />
+    <Node />
 </Svelvet>
