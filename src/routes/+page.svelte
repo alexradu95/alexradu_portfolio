9135dc8a028646eccd3aebb72b7a7f8ee7dd32bd
Radu Alexandru <alexradu95@proton.me>
2024-05-23 00:39:28 +0300

    Removed unused imports and code related to closing applications.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,7 @@
 <script>
-    import { openedApps } from '$lib/data/Applications.js';
-    import Panel from "$lib/components/panel/Panel.svelte";
-    import { scale } from 'svelte/transition';
-    import { onMount } from 'svelte';
-    import { Svelvet } from 'svelvet';
 
-    onMount(async () => {
-		$openedApps = [...$openedApps, {id: "", app: null}];
-	});
 </script>
 
-<div class="w-screen h-screen bg-gradient-to-b from-pink-500 to-cyan-500 relative" transition:scale={{ duration: 400 }}>
-    <Svelvet>
-        <Panel />
+<div class="w-screen h-screen bg-gradient-to-b from-pink-500 to-cyan-500 relative">
 
-        {#each $openedApps as openedApp}
-            <svelte:component this={openedApp.app} />
-        {/each}
-    </Svelvet>
 </div>
